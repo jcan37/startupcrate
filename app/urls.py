bb79b1c2d7b2ca28b69a072a83bb5cae65fdd0cf
Jeffrey Cannon <jmc5fm@virginia.edu>
2016-06-06 04:17:53 -0400

    Created initial templates and developed index page

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,9 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', 'app.views.index', name='index'),
+    url(r'^signup/', 'app.views.signup', name='signup'),
+    url(r'^signin/', 'app.views.signin', name='signin'),
+    url(r'^crate/', 'app.views.crate', name='crate'),
+    url(r'^subscriptions/', 'app.views.subscriptions', name='subscriptions'),
+    url(r'^settings/', 'app.views.settings', name='settings'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+def signup(request):
+    return render(request, 'app/signup.html')
+
+def signin(request):
+    return render(request, 'app/signin.html')
+
+def crate(request):
+    return render(request, 'app/crate.html')
+
+def subscriptions(request):
+    return render(request, 'app/subscriptions.html')
+
+def settings(request):
+    return render(request, 'app/settings.html')
